3dc865ec26008165404fe5c3664f39839481fc8c
Willam Bello <william_bm@hotmail.es>
2016-06-09 19:30:20 -0400

    remove unused references

--- BitbucketToGithub/Wrapper/Contracts/IConfigurationWrapper.cs
+++ BitbucketToGithub/Wrapper/Contracts/IConfigurationWrapper.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BitbucketToGithub.Wrapper.Contracts
+﻿namespace BitbucketToGithub.Wrapper.Contracts
 {
     public interface IConfigurationWrapper
     {
