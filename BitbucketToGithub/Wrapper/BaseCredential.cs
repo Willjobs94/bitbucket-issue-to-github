f6e80c8a49911925eeba25ac7051bec245b0bc66
Willam Bello <william_bm@hotmail.es>
2016-06-09 12:23:38 -0400

    add configuration manager wrapper

---
+++ BitbucketToGithub/Wrapper/BaseCredential.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using BitbucketToGithub.Wrapper.Contracts;
+
+namespace BitbucketToGithub.Wrapper
+{
+    public class BaseCredential : IBaseCredential
+    {
+        public BaseCredential(string credentialProviderName)
+        {
+            _credentialProviderName = credentialProviderName;
+        }
+
+        public string Key 
+            => ConfigurationManager.AppSettings[_credentialProviderName + "Key"];
+
+        public string Secret 
+            => ConfigurationManager.AppSettings[_credentialProviderName + "Secret"];
+
+        private readonly string _credentialProviderName;
+    }
+}
---
+++ BitbucketToGithub/Wrapper/BitBucketCredential.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BitbucketToGithub.Wrapper
+{
+    public class BitBucketCredential : BaseCredential
+    {
+        public BitBucketCredential() : base("BitBucket")
+        {
+        }
+
+    }
+}
