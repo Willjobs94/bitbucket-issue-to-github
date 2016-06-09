70b50d5d077335252c2c39468f8b3e86d8da9476
Willam Bello <william_bm@hotmail.es>
2016-06-09 18:54:27 -0400

    Add IoC Container

---
+++ BitbucketToGithub/App_Start/BitBucketToGithubModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BitbucketToGithub.Wrapper;
+using BitbucketToGithub.Wrapper.Contracts;
+using Ninject.Modules;
+
+namespace BitbucketToGithub.App_Start
+{
+    public class BitBucketToGithubModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IConfigurationWrapper>().To<ConfigurationWrapper>();
+        }
+    }
+}
